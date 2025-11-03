53c98b7244cc59272a378f41681f895b11660395
Артём Сампалов <sampalov167@gmail.com>
2025-11-03 08:34:15 +0300

    Setting up the Docker Compose orchestrator

--- DeduplicatorWorker/Program.cs
+++ DeduplicatorWorker/Program.cs
@@ -6,7 +6,6 @@ namespace DeduplicatorWorker
         {
             var builder = Host.CreateApplicationBuilder(args);
             builder.Services.AddHostedService<Worker>();
-
             var host = builder.Build();
             host.Run();
         }
